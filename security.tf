60287e2bdfe14d1fa39fc07162bf9d9a5a17bb85
Keshavkalyan <108111548+Keshavkalyan@users.noreply.github.com>
2025-07-22 17:02:11 +0530

    Update security.tf

--- security.tf
+++ security.tf
@@ -1,19 +1,41 @@
-resource "aws_security_group" "web_sg" {
-  name        = "web-sg"
-  description = "Allow HTTP inbound traffic"
-  vpc_id      = data.aws_vpc.default.id
+resource "aws_security_group" "helloworld" {
+  name        = "helloworld"
+  description = "Allow HTTP, HTTPS, and SSH"
+  vpc_id      = data.aws_vpc.default.id  # Reference to default VPC
 
   ingress {
+    description = "Allow HTTP"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Allow HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
+    description = "Allow all outbound"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "helloworld"
   }
 }
